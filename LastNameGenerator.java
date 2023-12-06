b78d0a9af34fb3e38300c3eb289409e297a5ded7
Elena Petrova <153084185+rear-parched@users.noreply.github.com>
2023-12-06 18:51:33 +0700

    Create LastNameGenerator.java

---
+++ LastNameGenerator.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class LastNameGenerator {
+    private List<String> lastNames;
+
+    public LastNameGenerator(List<String> lastNames) {
+        this.lastNames = lastNames;
+    }
+
+    public String generateLastName() {
+        // Implement logic to randomly select a last name from the list
+        // ...
+
+        return "Doe"; // Placeholder, replace with actual implementation
+    }
+}
