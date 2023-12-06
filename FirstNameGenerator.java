92e23cffd75ee17c015cecec704e8455453204b2
Elena Petrova <153084185+rear-parched@users.noreply.github.com>
2023-12-06 18:51:23 +0700

    Create FirstNameGenerator.java

---
+++ FirstNameGenerator.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class FirstNameGenerator {
+    private List<String> firstNames;
+
+    public FirstNameGenerator(List<String> firstNames) {
+        this.firstNames = firstNames;
+    }
+
+    public String generateFirstName() {
+        // Implement logic to randomly select a first name from the list
+        // ...
+
+        return "John"; // Placeholder, replace with actual implementation
+    }
+}
